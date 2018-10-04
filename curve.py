1836f694ca4cdcad8389a239208d73063d4d1b4f
jsg <jsg3001kr@daum.net>
2018-10-04 18:33:27 +0900

    drill8-1 2011182060 전승구 커밋횟수 5회 https://www.youtube.com/watch?v=hcgvDg0Y9GM
    https://www.youtube.com/watch?v=hcgvDg0Y9GM
    
    drill8-1 완성입니다.

--- curve.py
+++ curve.py
@@ -103,7 +103,7 @@ def draw_curve_4_points(p1, p2, p3, p4,p5):
         x = (2 * t ** 2 - 3 * t + 1) * p3[0] + (-4 * t ** 2 + 4 * t) * p4[0] + (2 * t ** 2 - t) * p5[0]
         y = (2 * t ** 2 - 3 * t + 1) * p3[1] + (-4 * t ** 2 + 4 * t) * p4[1] + (2 * t ** 2 - t) * p5[1]
         draw_point((x, y))
-    draw_point(p5)
+    draw_point(p5) 
 
 prepare_turtle_canvas()
 points = [(-300,200),(400,350),(300,-300),(-200,-200),(-300,200)]
