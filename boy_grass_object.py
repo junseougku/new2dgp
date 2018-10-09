3afe1266bb3919a9c6cac0b9b7c8b2adcef5cd59
jsg <jsg3001kr@daum.net>
2018-10-09 18:48:00 +0900

    drill9 기타오타 수정
    기타 오타수정하여 완성합니다

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import random
+
 class Ball:
     def __init__(self):
         self.x , self.y = random.randint(50,750),599
@@ -18,22 +19,22 @@ class Ball:
                 self.y -= self.speed
     def draw(self):
         self.image.draw(self.x,self.y)
+class Boy:
+    def __init__(self):
+        self.x, self.y = random.randint(100,700),90
+        self.frame = random.randint(0,7)
+        self.image = load_image('run_animation.png')
+    def update(self):
+        self.frame = (self.frame + 1 ) % 8
+        self.x += 5
+    def draw(self):
+        self.image.clip_draw(self.frame * 100 ,0,100,100,self.x,self.y)
+
 class Grass:
     def __init__(self):
         self.image = load_image('grass.png')
     def draw(self):
         self.image.draw(400,30)
-
-class Boy:
-    def __init__(self):
-        self.x , self.y = 0,90
-        self.frame = 0
-        self.image = load_image('run_animation.png')
-    def update(self):
-        self.frame = (self.frame + 1) % 8
-        self.x += 5
-    def draw(self):
-        self.image.clip_draw(self.frame*100,0,100,100,self.x,self.y)
 
 def handle_events():
     global running
@@ -41,20 +42,20 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+            close_canvas()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-# initialization code
+
 open_canvas()
+
 grass = Grass()
 team = [Boy() for i in range(11)]
 running = True
 
 balls = [Ball() for i in range(20)]
 
-
-# game main loop code
-while running:
+while running :
     handle_events()
     for boy in team:
         boy.update()
@@ -70,6 +71,8 @@ while running:
 
     update_canvas()
     delay(0.05)
-# finalization
+# initialization code
 
-close_canvas()
+# game main loop code
+
+# finalization code
